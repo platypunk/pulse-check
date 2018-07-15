5023299208d3b9352dbd709efaaece8678419ae8
Aiza Blanco <aiza.blanco@email.com>
2018-07-15 22:12:18 +1000

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pulse-check
+
+To run 
+1. npm install
+2. node Server.js
---
+++ Server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/groups",function(req,res){
+  res.sendFile(path + "groups.html");
+});
+
+router.get("/users",function(req,res){
+  res.sendFile(path + "users.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
