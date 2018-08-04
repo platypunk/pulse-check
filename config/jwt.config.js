3cb0f77d9ade2d8d5011df35970c5b4b5d5c39a2
Aiza Blanco <aiza.blanco@email.com>
2018-08-05 09:21:55 +1000

    Enable auth

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	enabled: true,
     secret: 'cowabunga',
     expiry: 1440 // 24 hours
 }
--- server.js
+++ server.js
@@ -35,26 +35,28 @@ router.use(function (req,res,next) {
 const users = require('./controllers/user.controller.js');
 router.post('/authenticate', users.authenticate);
 
-router.use(function (req, res, next) {
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-      if (err) {
-        return res.json({
-          success: false, 
-          message: 'Failed to authenticate token.' });    
-      } else {
-        req.decoded = decoded;    
-        next();
-      }
-    });
-  } else {
-    return res.status(403).send({ 
-      success: false, 
-      message: 'No token provided.'
-    });
-  }
-});
+if (jwtConfig.enabled) {
+  router.use(function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+      jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+        if (err) {
+          return res.json({
+            success: false, 
+            message: 'Failed to authenticate token.' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.'
+      });
+    }
+  });
+}
 
 app.use('/',router);
 
